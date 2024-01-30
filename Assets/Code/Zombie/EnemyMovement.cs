ca71f722069b6e3aa3f59ec05e03daadc5bcfe11
Otto hammarberg <Otto.hammarberg@elev.lbs.ga.se>
2024-01-30 09:26:14 +0100

    Movement zombie work

---
+++ Assets/Code/Zombie/EnemyMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private Transform target;
+    public float speed;
+    private float followRadius = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Calculate the distance between enemy and player
+        float distanceToTarget = Vector2.Distance(transform.position, target.position);
+
+        // Check if the distance is within the follow radius
+        if (distanceToTarget <= followRadius)
+        {
+            // Move towards the player
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+        // Optionally, you can add an else statement to define what the enemy does when the player is out of range.
+    }
+}
