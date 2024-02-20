707309671ae8ce29a02ef17437e37687bf92bbf6
Oscar Hermansson <oscar.hermansson@learnet.se>
2024-02-20 09:16:48 +0100

    herman

--- Assets/Code/Zombie/EnemyMovement.cs
+++ Assets/Code/Zombie/EnemyMovement.cs
@@ -8,39 +8,34 @@ public class EnemyMovement : MonoBehaviour
     public float speed;
     private float followRadius = 510.0f;
 
-    // Start is called before the first frame update
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // Calculate the distance between enemy and player
         float distanceToTarget = Vector2.Distance(transform.position, target.position);
 
-        // Check if the distance is within the follow radius
         if (distanceToTarget <= followRadius)
         {
-            // Move towards the player
             transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
         }
-        // Optionally, you can add an else statement to define what the enemy does when the player is out of range.
     }
+
     void OnCollisionEnter2D(Collision2D collision)
-{
-    if (collision.gameObject.CompareTag("Player"))
     {
-        PlayerData playerData = collision.gameObject.GetComponent<PlayerData>();
-        if (playerData != null)
+        if (collision.gameObject.CompareTag("Player"))
         {
-            playerData.TakeDamage(1);
-        }
-        else
-        {
-            Debug.LogError("PlayerData component not found on player object.");
+            PlayerData playerData = collision.gameObject.GetComponent<PlayerData>();
+            if (playerData != null)
+            {
+                playerData.TakeDamage(1); // Assuming TakeDamage method takes damage as argument
+            }
+            else
+            {
+                Debug.LogError("PlayerData component not found on player object.");
+            }
         }
     }
-}
 }
