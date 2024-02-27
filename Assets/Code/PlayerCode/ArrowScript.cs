a27f6be29acf4271da05fde63175cbf50c3d4d34
Oscar Hermansson <oscar.hermansson@learnet.se>
2024-02-27 10:22:38 +0100

    nya gerer

---
+++ Assets/Code/PlayerCode/ArrowScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowScript : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    private int groundLayer;
+    private int groundedArrowLayer;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        groundLayer = LayerMask.NameToLayer("Ground");
+        groundedArrowLayer = LayerMask.NameToLayer("GroundedArrow");
+
+        // Ensure the arrow starts on a different layer than GroundedArrow
+        // For example, it could be on the default layer or a custom "Arrow" layer
+        // gameObject.layer = LayerMask.NameToLayer("Arrow"); // Uncomment if using a custom "Arrow" layer
+    }
+
+    void Update()
+    {
+        if (!isGrounded)
+        {
+            TrackMovement();
+        }
+    }
+
+    void TrackMovement()
+    {
+        Vector2 dir = rb.velocity;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
